a047d391a43ef04d1e9a1414a075b92e6e8afc21
Josef <croymen@seznam.cz>
2025-09-14 18:24:27 +0200

    kube deployment

--- release.config.js
+++ release.config.js
@@ -9,7 +9,7 @@ module.exports = {
         ["@semantic-release/git",
             {
                 "assets": ["CHANGELOG.md"],
-                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+                "message": "chore(release): latest [skip ci]\n\n${nextRelease.notes}"
             }],
         "semantic-release-docker-buildx",
         ["@semantic-release/github", {"addReleases": "bottom"}]
